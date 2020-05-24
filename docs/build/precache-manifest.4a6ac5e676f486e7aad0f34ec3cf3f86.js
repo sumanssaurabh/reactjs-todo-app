98ca6933e452d2b9ee212f9c2cb9173993afec32
Suman Saurabh <30201452+sumanssaurabh@users.noreply.github.com>
2020-05-24 16:47:11 +0530

    added homepage

---
+++ docs/build/precache-manifest.4a6ac5e676f486e7aad0f34ec3cf3f86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411fb478259e36193eaa2c49b68b04c9",
+    "url": "https://sumanssaurabh.github.io/reactjs-todo-app/index.html"
+  },
+  {
+    "revision": "430aa3ee596d4e1659d4",
+    "url": "https://sumanssaurabh.github.io/reactjs-todo-app/static/js/2.a642793a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://sumanssaurabh.github.io/reactjs-todo-app/static/js/2.a642793a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdcda275b74efea24340",
+    "url": "https://sumanssaurabh.github.io/reactjs-todo-app/static/js/main.29424bf3.chunk.js"
+  },
+  {
+    "revision": "d4f3caf1620ca24d68ef",
+    "url": "https://sumanssaurabh.github.io/reactjs-todo-app/static/js/runtime-main.8fb9dd9f.js"
+  }
+]);
