e4e48213516ace64a4dfce40ecb45220589b6062
Chetan Chadha <chetanchadha1234@gmail.com>
2020-10-01 00:05:19 +0530

    Updated to functional based components

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
-export default class Todos extends Component {
-  render() {
-    return this.props.todos.map((todo) => (
-      <TodoItem
-        key={todo.id}
-        todo={todo}
-        markComplete={this.props.markComplete}
-        delTodo={this.props.delTodo}
-      />
-    ));
-  }
+// export default class Todos extends Component {
+//   render() {
+//     return this.props.todos.map((todo) => (
+//       <TodoItem
+//         key={todo.id}
+//         todo={todo}
+//         markComplete={this.props.markComplete}
+//         delTodo={this.props.delTodo}
+//       />
+//     ));
+//   }
+// }
+
+export default function Todos(props) {
+  return (
+    <span>
+      {props.todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          markComplete={props.markComplete}
+          delTodo={props.delTodo}
+        />
+      ))}
+    </span>
+  );
 }
