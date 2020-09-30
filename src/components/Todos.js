78b01e130bca5cdcf26e601f83dabe1f20570ed9
Supantha Paul <codewithharsh@gmail.com>
2020-10-01 00:40:15 +0530

    Changed Todos.js to functional component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
-export default class Todos extends Component {
-  render() {
-    return this.props.todos.map((todo) => (
+const Todos = ({todos, markComplete, delTodo}) => {
+    return todos.map((todo) => (
       <TodoItem
         key={todo.id}
         todo={todo}
-        markComplete={this.props.markComplete}
-        delTodo={this.props.delTodo}
+        markComplete={markComplete}
+        delTodo={delTodo}
       />
     ));
-  }
 }
+
+export default Todos;
